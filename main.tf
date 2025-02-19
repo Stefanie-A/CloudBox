df996d6ec5a3999cf31381c7d47696d6ecf94973
Stefanie-A <albertstephanie630@gmail.com>
2025-02-19 21:37:05 +0000

    feat: s3 bucket for file upload

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.8"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.s3_bucket_name
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_encryption" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.s3_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
