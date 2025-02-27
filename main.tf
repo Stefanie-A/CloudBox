24f39b6143cc8e256df2e46e4da5d0dc56f0c92a
Stefanie-A <albertstephanie630@gmail.com>
2025-02-27 22:18:37 +0000

    feat: add cloudfront distribution

--- main.tf
+++ main.tf
@@ -160,3 +160,45 @@ resource "aws_dynamodb_table" "dynamodb_table" {
     write_capacity  = 1
   }
 }
+
+#cloudfront
+resource "aws_cloudfront_distribution" "cloudfront_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id       = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
+    viewer_protocol_policy = "allow-all"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "Allow CloudFront to reach the bucket"
+}
