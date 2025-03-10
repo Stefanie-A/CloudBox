f5ae31993060a82747046173fcf0ec672c0acece
Stefanie-A <albertstephanie630@gmail.com>
2025-03-10 15:11:26 +0000

    chore: modify ecr repository

--- main.tf
+++ main.tf
@@ -376,9 +376,12 @@ resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
 #ECR
 resource "aws_ecr_repository" "ecr_repository" {
   name                 = var.ecr_repository_name
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
+    encryption_configuration {
+    encryption_type = "AES256"
+    }
 }
