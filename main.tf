1a562ffd659fa72bc051603bee2ba14c0c16ddfc
Stefanie-A <albertstephanie630@gmail.com>
2025-03-21 14:52:09 +0000

    chore: romove callback urls

--- main.tf
+++ main.tf
@@ -406,7 +406,6 @@ resource "aws_cognito_user_pool_client" "user_pool_client" {
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_flows                  = ["code", "implicit"]
   allowed_oauth_scopes                 = ["email", "openid", "profile"]
-  callback_urls                        = ["https://localhost:3000"]
   supported_identity_providers         = ["COGNITO"]
 }
 
