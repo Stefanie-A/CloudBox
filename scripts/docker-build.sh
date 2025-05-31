9ab4ede8aa6d3d7a8dcc17e4ff8c2e05fcd1382f
Stefanie-A <albertstephanie630@gmail.com>
2025-05-31 18:41:39 +0100

    feat: push image to docker hub

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -4,4 +4,4 @@ sudo add-apt-repository ppa:cncf-buildpacks/pack-cli
 sudo apt-get update
 sudo apt-get install pack-cli
 cd app
-pack build cloudbox:latest --builder paketobuildpacks/builder:base
+pack build $DOCKER_USERNAME/cloudbox:latest --builder paketobuildpacks/builder:base
