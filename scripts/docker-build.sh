72f8315f04ead482a3f196335aaf8631a6f7f95f
Stefanie-A <albertstephanie630@gmail.com>
2025-05-31 18:52:18 +0100

    feat: push image to docker hub

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$DOCKER_USERNAME" ]; then
+  echo "DOCKER_USERNAME is not set!"
+  exit 1
+fi
+
 sudo add-apt-repository ppa:cncf-buildpacks/pack-cli
 sudo apt-get update
 sudo apt-get install pack-cli
